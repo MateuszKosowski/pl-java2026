ea0495d38a1084433116bc8396b75cd1f8c805fd
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2026-03-27 19:15:51 +0100

    Implement authentication with JWT: add AuthController, JwtService, and user login functionality

---
+++ auth-server/src/main/java/pl/zzpj/auth_server/service/JwtService.java
@@ -0,0 +1,23 @@
+package pl.zzpj.auth_server.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("${app.jwt.secret}")
+    private String secretKey;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 86400000)) // 24h
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .compact();
+    }
+}
